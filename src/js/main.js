84fdc9688802fd05438f05534635adcb92e20e3e
popoolahussein <popoolakehindehussein@gmail.com>
2024-07-06 23:46:25 +0100

    chore: added nit set-up

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+let app = "";
+
+function createApp() {
+  app.innerHTML += `Let's create mobile app!`
+};
+
+const appSettings = {
+  databaseURL: "https://mobile-app-3a7fa-default-rtdb.europe-west1.firebasedatabase.app/"
+}
